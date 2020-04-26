c9c7be1a5fd08977cd6b69b97f9f85fe341493e4
Nicolás Garay <nicolasmgaray@gmail.com>
2020-04-26 17:25:18 -0300

    new web build

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -7,13 +7,10 @@ const RESOURCES = {
 "assets/images/logo.png": "837c4288713dbe56af119bf799ccedee",
 "assets/LICENSE": "42456637dffc87c9762dcefc26b37e9f",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "9a62a954b81a1ad45a58b9bcea89b50b",
-"favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "682a0cd2485d7e694ee7f006bbac2023",
 "/": "682a0cd2485d7e694ee7f006bbac2023",
 "main.dart.js": "cf3ee952fcb765a028e453d4a17737c0",
-"manifest.json": "397d62aa2a79b9bdef5ce3bd85b6286b"
+"manifest.json": "801d742fbbd63319f5042a6e16f2557c"
 };
 
 self.addEventListener('activate', function (event) {
--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class FlashChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.light().copyWith(),
       initialRoute: WelcomeScreen.route,
       routes: {
